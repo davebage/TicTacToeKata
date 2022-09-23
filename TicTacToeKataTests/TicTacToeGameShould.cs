6149ff6cb43a92713fc545a99e28251abdaa853e
Matthew King <matthew.king@codurance.com>
2022-09-23 16:53:24 +0100

    Small refactor

--- TicTacToeKataTests/TicTacToeGameShould.cs
+++ TicTacToeKataTests/TicTacToeGameShould.cs
@@ -7,56 +7,45 @@ namespace TicTacToeKataTests
     public class TicTacToeGameShould
     {
         private TicTacToeGame _ticTacToeGame;
-
-
+        
         [SetUp]
         public void Setup()
         {
-
+            _ticTacToeGame = new TicTacToeGame();
         }
         
         [Test]
         public void Prevent_O_From_Going_First()
         {
-            var ticTacToeGame = new TicTacToeGame();
-
-            Assert.Throws<InvalidOperationException>(() => ticTacToeGame.PlaceToken("O", 0, 0));
+            Assert.Throws<InvalidOperationException>(() => _ticTacToeGame.PlaceToken("O", 0, 0));
         }
 
         [Test]
         public void Allow_X_To_Go_First()
         {
-            var ticTacToeGame = new TicTacToeGame();
-
-            ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, 0);
         }
 
         [Test]
         public void Prevent_X_From_Going_Twice_In_A_Row()
         {
-            var ticTacToeGame = new TicTacToeGame();
-        
-            ticTacToeGame.PlaceToken("X", 0, 0);
-            Assert.Throws<InvalidOperationException>(() => ticTacToeGame.PlaceToken("X", 0, 0));
+            _ticTacToeGame.PlaceToken("X", 0, 0);
+            Assert.Throws<InvalidOperationException>(() => _ticTacToeGame.PlaceToken("X", 0, 0));
         }
 
         [Test]
         public void Prevent_O_From_Going_Twice_In_A_Row()
         {
-            var ticTacToeGame = new TicTacToeGame();
-
-            ticTacToeGame.PlaceToken("X", 0, 0);
-            ticTacToeGame.PlaceToken("O", 0, 0);
-            Assert.Throws<InvalidOperationException>(() => ticTacToeGame.PlaceToken("O", 0, 0));
+            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("O", 0, 0);
+            Assert.Throws<InvalidOperationException>(() => _ticTacToeGame.PlaceToken("O", 0, 0));
         }
 
         [Test]
         public void Allow_O_To_Be_Placed_Second()
         {
-            var ticTacToeGame = new TicTacToeGame();
-
-            ticTacToeGame.PlaceToken("X", 0, 0);
-            ticTacToeGame.PlaceToken("O", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("O", 0, 0);
 
             // Assert no exception is thrown.
         }
@@ -64,11 +53,9 @@ namespace TicTacToeKataTests
         [Test]
         public void Allow_X_To_Be_Placed_Third()
         {
-            var ticTacToeGame = new TicTacToeGame();
-
-            ticTacToeGame.PlaceToken("X", 0, 0);
-            ticTacToeGame.PlaceToken("O", 0, 1);
-            ticTacToeGame.PlaceToken("X", 0, 2);
+            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("O", 0, 1);
+            _ticTacToeGame.PlaceToken("X", 0, 2);
 
             // Assert no exception is thrown.
         }
@@ -76,11 +63,9 @@ namespace TicTacToeKataTests
         [Test]
         public void Prevent_X_Being_Placed_in_An_Occupied_Position()
         {
-            var ticTacToeGame = new TicTacToeGame();
-
-            ticTacToeGame.PlaceToken("X", 0, 0);
-            ticTacToeGame.PlaceToken("O", 0, 1);
-            Assert.Throws<InvalidOperationException>(() => ticTacToeGame.PlaceToken("X", 0, 0));
+            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("O", 0, 1);
+            Assert.Throws<InvalidOperationException>(() => _ticTacToeGame.PlaceToken("X", 0, 0));
         }
     }
 }
