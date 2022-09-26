b169eeb9c84c0561022c82393167b5f04b7772c3
Dave Bage <david_bage@hotmail.com>
2022-09-26 17:23:08 +0100

    Create new NoWinnerException

---
+++ Tic_Tac_Toe_Kata/NoWinnerException.cs
@@ -0,0 +1,13 @@
+﻿namespace Tic_Tac_Toe_Kata;
+
+public class NoWinnerException : Exception
+{
+    public NoWinnerException()
+    {
+            
+    }
+
+    public NoWinnerException(string message) : base(message)
+    {
+    }
+}
