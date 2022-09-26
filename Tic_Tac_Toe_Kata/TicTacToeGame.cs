7f729af79c82023285c33331e518cd64e670def2
Dave Bage <david_bage@hotmail.com>
2022-09-26 15:51:44 +0100

    Added tests for wins in middle column

--- Tic_Tac_Toe_Kata/TicTacToeGame.cs
+++ Tic_Tac_Toe_Kata/TicTacToeGame.cs
@@ -30,6 +30,8 @@
         {
             if (_board[0, 0] == token && _board[0, 1] == token && _board[0, 2] == token )
                 return true;
+            if (_board[1, 0] == token && _board[1, 1] == token && _board[1, 2] == token)
+                return true;
 
 
             if (_board[0, 2] == token && _board[1, 2] == token && _board[2, 2] == token)
