00cbb8f588c2acea47308d9ae266c240b8252865
Dave Bage <david_bage@hotmail.com>
2022-09-26 15:05:43 +0100

    Change PlaceToken parameters from xCoordinate, yCoordinate to column, row respectively

--- Tic_Tac_Toe_Kata/TicTacToeGame.cs
+++ Tic_Tac_Toe_Kata/TicTacToeGame.cs
@@ -5,20 +5,20 @@
         private string _lastToken = string.Empty;
         private string[,] _board = new string[3, 3];
 
-        public void PlaceToken(string token, int xCoordinate, int yCoordinate)
+        public void PlaceToken(string token, int column, int row)
         {
-            if (!string.IsNullOrEmpty(_board[xCoordinate, yCoordinate])) throw new PositionAlreadyOccupiedException();
+            if (!string.IsNullOrEmpty(_board[column, row])) throw new PositionAlreadyOccupiedException();
 
             if (token == "X" && token != _lastToken)
             {
-                _board[xCoordinate, yCoordinate] = token;
+                _board[column, row] = token;
                 _lastToken = token;
                 return;
             }
 
             if (token == "O" && token != _lastToken && _lastToken != string.Empty)
             {
-                _board[xCoordinate, yCoordinate] = token;
+                _board[column, row] = token;
                 _lastToken = token;
                 return;
             }
