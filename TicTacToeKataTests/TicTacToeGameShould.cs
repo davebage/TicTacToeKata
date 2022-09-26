d0d341c3121d50490c0e3c8cc137b48a0279fd00
Dave Bage <david_bage@hotmail.com>
2022-09-26 15:08:27 +0100

    Add constant values to make tests more readable

--- TicTacToeKataTests/TicTacToeGameShould.cs
+++ TicTacToeKataTests/TicTacToeGameShould.cs
@@ -8,7 +8,11 @@ namespace TicTacToeKataTests
     {
 
         private TicTacToeGame _ticTacToeGame;
-        
+
+        private const int BOTTOM_ROW = 0;
+        private const int MIDDLE_ROW = 1;
+        private const int TOP_ROW = 2;
+
         [SetUp]
         public void Setup()
         {
@@ -19,19 +23,19 @@ namespace TicTacToeKataTests
         public void Prevent_O_From_Going_First()
         {
             Assert.Throws<TokenPlacedOutOfOrderException>(() => 
-                _ticTacToeGame.PlaceToken("O", 0, 0));
+                _ticTacToeGame.PlaceToken("O", 0, BOTTOM_ROW));
         }
 
         [Test]
         public void Allow_X_To_Go_First()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
         }
 
         [Test]
         public void Prevent_X_From_Going_Twice_In_A_Row()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             Assert.Throws<TokenPlacedOutOfOrderException>(() =>
                 _ticTacToeGame.PlaceToken("X", 0, 1));
         }
@@ -39,7 +43,7 @@ namespace TicTacToeKataTests
         [Test]
         public void Prevent_O_From_Going_Twice_In_A_Row()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             _ticTacToeGame.PlaceToken("O", 0, 1);
             Assert.Throws<TokenPlacedOutOfOrderException>(() => 
                 _ticTacToeGame.PlaceToken("O", 0, 2));
@@ -48,7 +52,7 @@ namespace TicTacToeKataTests
         [Test]
         public void Allow_O_To_Be_Placed_Second()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             _ticTacToeGame.PlaceToken("O", 0, 1);
 
             // Assert no exception is thrown.
@@ -57,7 +61,7 @@ namespace TicTacToeKataTests
         [Test]
         public void Allow_X_To_Be_Placed_Third()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             _ticTacToeGame.PlaceToken("O", 0, 1);
             _ticTacToeGame.PlaceToken("X", 0, 2);
 
@@ -67,25 +71,25 @@ namespace TicTacToeKataTests
         [Test]
         public void Prevent_X_Being_Placed_in_An_Occupied_Position()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             _ticTacToeGame.PlaceToken("O", 0, 1);
             Assert.Throws<PositionAlreadyOccupiedException>(() => 
-                _ticTacToeGame.PlaceToken("X", 0, 0));
+                _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW));
         }
 
         [Test]
         public void Prevent_O_Being_Placed_in_An_Occupied_Position()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             Assert.Throws<PositionAlreadyOccupiedException>(() =>
-                _ticTacToeGame.PlaceToken("O", 0, 0));
+                _ticTacToeGame.PlaceToken("O", 0, BOTTOM_ROW));
         }
 
         [Test]
         public void Mark_X_As_Winner_On_Top_Row_After_Three_Moves()
         {
             _ticTacToeGame.PlaceToken("X", 0, 2);
-            _ticTacToeGame.PlaceToken("O", 0, 0);
+            _ticTacToeGame.PlaceToken("O", 0, BOTTOM_ROW);
             _ticTacToeGame.PlaceToken("X", 1, 2);
             _ticTacToeGame.PlaceToken("O", 0, 1);
             _ticTacToeGame.PlaceToken("X", 2, 2);
@@ -99,7 +103,7 @@ namespace TicTacToeKataTests
             _ticTacToeGame.PlaceToken("X", 0, 2);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
-            _ticTacToeGame.PlaceToken("O", 0, 0);
+            _ticTacToeGame.PlaceToken("O", 0, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("O"));
 
             _ticTacToeGame.PlaceToken("X", 1, 2);
@@ -115,7 +119,7 @@ namespace TicTacToeKataTests
         [Test]
         public void Mark_O_As_Winner_On_Top_Row_After_Three_Moves_And_Not_Before()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
             _ticTacToeGame.PlaceToken("O", 0, 2);
@@ -127,7 +131,7 @@ namespace TicTacToeKataTests
             _ticTacToeGame.PlaceToken("O", 1, 2);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("O"));
 
-            _ticTacToeGame.PlaceToken("X", 2, 0);
+            _ticTacToeGame.PlaceToken("X", 2, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
             _ticTacToeGame.PlaceToken("O", 2, 2);
@@ -141,7 +145,7 @@ namespace TicTacToeKataTests
             _ticTacToeGame.PlaceToken("X", 0, 1);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
-            _ticTacToeGame.PlaceToken("O", 0, 0);
+            _ticTacToeGame.PlaceToken("O", 0, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("O"));
 
             _ticTacToeGame.PlaceToken("X", 1, 1);
@@ -157,13 +161,13 @@ namespace TicTacToeKataTests
         [Test]
         public void Mark_O_As_Winner_On_Middle_Row_After_Three_Moves_And_Not_Before()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
             _ticTacToeGame.PlaceToken("O", 0, 1);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("O"));
 
-            _ticTacToeGame.PlaceToken("X", 1, 0);
+            _ticTacToeGame.PlaceToken("X", 1, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
             _ticTacToeGame.PlaceToken("O", 1, 1);
@@ -180,19 +184,19 @@ namespace TicTacToeKataTests
         [Test]
         public void Mark_X_As_Winner_On_Bottom_Row_After_Three_Moves_And_Not_Before()
         {
-            _ticTacToeGame.PlaceToken("X", 0, 0);
+            _ticTacToeGame.PlaceToken("X", 0, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
             _ticTacToeGame.PlaceToken("O", 0, 1);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("O"));
 
-            _ticTacToeGame.PlaceToken("X", 1, 0);
+            _ticTacToeGame.PlaceToken("X", 1, BOTTOM_ROW);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("X"));
 
             _ticTacToeGame.PlaceToken("O", 0, 2);
             Assert.IsFalse(_ticTacToeGame.CheckWinner("O"));
 
-            _ticTacToeGame.PlaceToken("X", 2, 0);
+            _ticTacToeGame.PlaceToken("X", 2, BOTTOM_ROW);
             Assert.IsTrue(_ticTacToeGame.CheckWinner("X"));
         }
 
